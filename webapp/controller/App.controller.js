c5afa56c7f24a058aa0a5fe5761173cc190438d0
tuncmehmet <84574458+tuncmehmet@users.noreply.github.com>
2025-05-04 19:02:24 +0300

    Add initial implementation of invoice and hello panel views, controllers, and related resources

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,20 +1,8 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel"
- ], (Controller, MessageToast, JSONModel, ResourceModel ) => {
-    "use strict";
- 
-    return Controller.extend("ui5.walkthrough.controller.App", {
-       onShowHello() {
-        // read msg from i18n model
-         const oBundle = this.getView().getModel("i18n").getResourceBundle();
-         const sRecipient = this.getView().getModel().getProperty("/recipient/name");
-         const sMsg = oBundle.getText("helloMsg", [sRecipient]);
+   "sap/ui/core/mvc/Controller"
+], (Controller) => {
+   "use strict";
 
-         // show message
-         MessageToast.show(sMsg);
-       }
-    });
- });
+   return Controller.extend("ui5.walkthrough.controller.App", {
+   });
+});
