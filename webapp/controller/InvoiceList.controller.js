13a55647cca0b05d76cf326c63a0c073eea2e2ea
tuncmehmet <84574458+tuncmehmet@users.noreply.github.com>
2025-05-18 02:36:25 +0300

    Enhance UI5 application with new views, middleware, and filtering functionality

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -1,7 +1,9 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/ui/model/json/JSONModel"
-], (Controller, JSONModel) => {
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+], (Controller, JSONModel, Filter, FilterOperator) => {
 	"use strict";
 
 	return Controller.extend("ui5.walkthrough.controller.InvoiceList", {
@@ -10,6 +12,44 @@ sap.ui.define([
 				currency: "EUR"
 			});
 			this.getView().setModel(oViewModel, "view");
+		},
+
+		onFilterInvoices(oEvent) {
+			// build filter array
+			const aFilter = [];
+			const sQuery = oEvent.getParameter("query");
+			if (sQuery) {
+				aFilter.push(new Filter("ProductName", FilterOperator.Contains, sQuery));
+			}
+
+			// filter binding
+			const oList = this.byId("invoiceList"); // viewdeki invoiceList id'sine ait olan elementi(list) al
+			const oBinding = oList.getBinding("items");//list içindeki items verisini çek
+			oBinding.filter(aFilter);// gelen veriyi filtrele
+		},
+		onPress() {
+			const oRouter = this.getOwnerComponent().getRouter();
+			oRouter.navTo("detail");
 		}
+		
+		// ÇOKLU FILTRELEME
+		// onFilterInvoices() {
+		// 	// build filter array
+		// 	const aFilter = [];
+		// 	const sQuery1 = this.byId("search1").getValue();
+		// 	if (sQuery1) {
+		// 		aFilter.push(new Filter("ProductName", FilterOperator.Contains, sQuery1));
+		// 	}
+		
+		// 	const sQuery2 = this.byId("search2").getValue();
+		// 	if (sQuery2) {
+		// 		aFilter.push(new Filter("ExtendedPrice", FilterOperator.GT, sQuery2));
+		// 	}
+		
+		// 	// filter binding
+		// 	const oList = this.byId("invoiceList");
+		// 	const oBinding = oList.getBinding("items");
+		// 	oBinding.filter(aFilter);
+		// }
 	});
 });
